f82da6d79b5dd63de52d1362ead989dbf1d719c1
Dipo <mstrdipo@gmail.com>
2020-03-23 17:54:57 +0000

    end

---
+++ New Unity Project/Assets/Scripts/MyScript/ending.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ending : MonoBehaviour
+{
+    public GameObject endPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        endPanel.SetActive(true);
+    }
+}
